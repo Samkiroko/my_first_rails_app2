00863cbda96126dca4fa848b50eca0481c62a1df
Sam <sam@Sams-MacBook-Pro.local>
2020-05-05 21:55:57 +0300

    updates for heroku deployment

--- .history/config/routes_20200505213446.rb
+++ .history/config/routes_20200505213446.rb
@@ -1,7 +1,4 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  root 'car#index'
   resources :cars
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
